2ad6cb8f8088d3e10d48aca7327194032eb97807
Lumiere Xyloto <740207968@qq.com>
2018-10-16 22:48:50 +0800

    add digui

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,40 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '票型一'
+        }, {
+          title: '票型二'
+        }, {
+          title: '票型三'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }]
+    }
   }
 }
 </script>
